93f95111cf8270d6d40453f0c25f229c314cef6b
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-12-16 11:12:32 -0300

    requisito 2 continuação

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ pallete.style.width = '19%';
 pallete.style.margin = '12px auto';
 
 ;
+
+for (let index = 0; index < 4; index += 1) {
+    const color = document.createElement('div');
+    color.className = 'color';
+    addPallete.appendChild(color);
+}
+
+const palleteElement = document.getElementsByClassName('color');
+for (let index = 0; index < 4; index += 1) {
+    palleteElement[index].style.border = "1px solid black";
+    palleteElement[index].style.padding = '30px';
+    palleteElement[index].style.display = 'inline-block';
+}
+palleteElement[1].style.backgroundColor = 'blue';
+palleteElement[2].style.backgroundColor = 'yellow';
+palleteElement[3].style.backgroundColor = 'red';
+
+
